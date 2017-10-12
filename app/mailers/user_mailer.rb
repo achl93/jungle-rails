cf582654c6ea3db0e6c6e256a7317e5b2b858ea1
Andy Leung <andy_ch_009@hotmail.com>
2017-10-12 21:28:16 +0000

    user email with web opener

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,9 +1,10 @@
 class UserMailer < ApplicationMailer
   default from: "no-repy@jungle.com"
 
-  def receipt_email(user_email, order_id)
-    @user_email = user_email
-    @order_id = order_id
+  def receipt_email(order)
+    @user_email = order.email
+    @order_id = order.id
+    @line_items = order.line_items
     mail(to: @user_email, subject: 'Confirmation for order #: ' + @order_id.to_s)
   end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,17 +15,12 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_options = { from: "no-reply@jungle.com" }
-  config.action_mailer.smtp_settings = {
-    address:              'smtp.gmail.com',
-    port:                 587,
-    domain:               'jungle.com',
-    user_name:            ENV["EMAIL"],
-    password:             ENV["EMAIL_PW"],
-    authentication:       'plain',
-    enable_starttls_auto: true  }
+  # config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :letter_opener_web
+  # LetterOpenerWeb.configure do |config|
+  #   config.letters_location = Rails.root.join('your', 'new', 'path')
+  # end
+  # config.action_mailer.default_options = { from: "no-reply@jungle.com" }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
